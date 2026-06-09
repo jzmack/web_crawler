0684d7f5895746ebdf892dbb5ce1d03b57cf05ed
Jake Mackin <canesfanjake1@gmail.com>
2026-06-09 13:51:48 -0400

    added basic CLI argument logic

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from web-crawler!")
+import sys
 
+def main():
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    if len(sys.argv) == 2:
+        BASE_URL = sys.argv[1]
+        print(f"starting crawl of: {BASE_URL}")
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
