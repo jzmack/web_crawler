d9e0b5f231497777cd507a1b861854f65e4e1ff2
Jake Mackin <canesfanjake1@gmail.com>
2026-06-09 10:12:58 -0400

    added HTML parsing for h1, h2, and p tags with unit tests, and yes they are passing

--- crawl.py
+++ crawl.py
@@ -1,11 +1,34 @@
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup, Tag
 
 def normalize_url(url: str) -> str:
     parsed_url = urlparse(url)
-    print("URL=" + url)
+    #print("URL=" + url)
     normalized_string = (parsed_url.netloc + parsed_url.path).rstrip("/").strip().lower()
-    print(normalized_string)
+    #print(normalized_string)
     return normalized_string 
 
-def get_heading_from_html(url: str) -> str:
-    return
+def get_heading_from_html(html: str) -> str:
+    html_doc = html
+    soup = BeautifulSoup(html_doc, "html.parser")
+    heading = soup.find("h1")
+    if heading == None:
+        heading = soup.find("h2")
+        if heading == None:
+            return ""
+        return heading.get_text(strip=True)
+    return heading.get_text(strip=True)
+
+def get_first_paragraph_from_html(html: str) -> str:
+    html_doc = html
+    soup = BeautifulSoup(html_doc, "html.parser")
+    main_sect = soup.find("main")
+    if main_sect != None:
+        first_p = main_sect.find("p")
+        if first_p == None:
+            return ""
+        return first_p.get_text(strip=True)
+    first_p = soup.find("p")
+    if first_p == None:
+        return ""
+    return first_p.get_text(strip=True)
--- test_crawl.py
+++ test_crawl.py
@@ -1,5 +1,5 @@
 import unittest
-from crawl import normalize_url, get_heading_from_html
+from crawl import normalize_url, get_heading_from_html, get_first_paragraph_from_html
 
 class TestCrawl(unittest.TestCase):
     def test_normalize_url(self):
@@ -70,6 +70,36 @@ class TestCrawl(unittest.TestCase):
                     </html>
                     """
         actual = get_heading_from_html(input_html)
+        expected = ""
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_main(self):
+        input_body = '''<html><body>
+                        <p>Outside paragraph.</p>
+                        <main>
+                            <p>Main paragraph.</p>
+                        </main>
+                        </body></html>'''
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "Main paragraph."
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_no_main(self):
+        input_body ="""
+                    <html><body>
+                    <p>Outside paragraph.</p>
+                    </body></html>
+                    """
+        actual = get_first_paragraph_from_html(input_body)
+        expected = "Outside paragraph."
+        self.assertEqual(actual, expected)
+    
+    def test_no_p(self):
+        input_body ="""
+                    <html><body> Girl look at that body
+                    </body></html>
+                    """
+        actual = get_first_paragraph_from_html(input_body)
         expected = ""
         self.assertEqual(actual, expected)
 
